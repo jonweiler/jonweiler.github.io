d0d8678d3927ae186a98783c6a8d746910b7dae3
JonWeiler <jonw@professionalmarksmen.com>
2015-09-17 15:05:09 -0500

    update website

--- js/main.js
+++ js/main.js
@@ -1,1 +1,213 @@
-/*! skinny-bones-jekyll - v0.0.1 - 2015-03-02 */!function($){"use strict";$.fn.fitVids=function(options){var settings={customSelector:null,ignore:null};if(!document.getElementById("fit-vids-style")){var head=document.head||document.getElementsByTagName("head")[0],css=".fluid-width-video-wrapper{width:100%;position:relative;padding:0;}.fluid-width-video-wrapper iframe,.fluid-width-video-wrapper object,.fluid-width-video-wrapper embed {position:absolute;top:0;left:0;width:100%;height:100%;}",div=document.createElement("div");div.innerHTML='<p>x</p><style id="fit-vids-style">'+css+"</style>",head.appendChild(div.childNodes[1])}return options&&$.extend(settings,options),this.each(function(){var selectors=["iframe[src*='player.vimeo.com']","iframe[src*='youtube.com']","iframe[src*='youtube-nocookie.com']","iframe[src*='kickstarter.com'][src*='video.html']","object","embed"];settings.customSelector&&selectors.push(settings.customSelector);var ignoreList=".fitvidsignore";settings.ignore&&(ignoreList=ignoreList+", "+settings.ignore);var $allVideos=$(this).find(selectors.join(","));$allVideos=$allVideos.not("object object"),$allVideos=$allVideos.not(ignoreList),$allVideos.each(function(){var $this=$(this);if(!($this.parents(ignoreList).length>0||"embed"===this.tagName.toLowerCase()&&$this.parent("object").length||$this.parent(".fluid-width-video-wrapper").length)){$this.css("height")||$this.css("width")||!isNaN($this.attr("height"))&&!isNaN($this.attr("width"))||($this.attr("height",9),$this.attr("width",16));var height="object"===this.tagName.toLowerCase()||$this.attr("height")&&!isNaN(parseInt($this.attr("height"),10))?parseInt($this.attr("height"),10):$this.height(),width=isNaN(parseInt($this.attr("width"),10))?$this.width():parseInt($this.attr("width"),10),aspectRatio=height/width;if(!$this.attr("id")){var videoID="fitvid"+Math.floor(999999*Math.random());$this.attr("id",videoID)}$this.wrap('<div class="fluid-width-video-wrapper"></div>').parent(".fluid-width-video-wrapper").css("padding-top",100*aspectRatio+"%"),$this.removeAttr("height").removeAttr("width")}})})}}(window.jQuery||window.Zepto),function($){var $w=$(window);$.fn.visible=function(partial,hidden,direction){if(!(this.length<1)){var $t=this.length>1?this.eq(0):this,t=$t.get(0),vpWidth=$w.width(),vpHeight=$w.height(),direction=direction?direction:"both",clientSize=hidden===!0?t.offsetWidth*t.offsetHeight:!0;if("function"==typeof t.getBoundingClientRect){var rec=t.getBoundingClientRect(),tViz=rec.top>=0&&rec.top<vpHeight,bViz=rec.bottom>0&&rec.bottom<=vpHeight,lViz=rec.left>=0&&rec.left<vpWidth,rViz=rec.right>0&&rec.right<=vpWidth,vVisible=partial?tViz||bViz:tViz&&bViz,hVisible=partial?lViz||lViz:lViz&&rViz;if("both"===direction)return clientSize&&vVisible&&hVisible;if("vertical"===direction)return clientSize&&vVisible;if("horizontal"===direction)return clientSize&&hVisible}else{var viewTop=$w.scrollTop(),viewBottom=viewTop+vpHeight,viewLeft=$w.scrollLeft(),viewRight=viewLeft+vpWidth,offset=$t.offset(),_top=offset.top,_bottom=_top+$t.height(),_left=offset.left,_right=_left+$t.width(),compareTop=partial===!0?_bottom:_top,compareBottom=partial===!0?_top:_bottom,compareLeft=partial===!0?_right:_left,compareRight=partial===!0?_left:_right;if("both"===direction)return!!clientSize&&viewBottom>=compareBottom&&compareTop>=viewTop&&viewRight>=compareRight&&compareLeft>=viewLeft;if("vertical"===direction)return!!clientSize&&viewBottom>=compareBottom&&compareTop>=viewTop;if("horizontal"===direction)return!!clientSize&&viewRight>=compareRight&&compareLeft>=viewLeft}}}}(jQuery),$(document).ready(function(){$("#js-menu-trigger,#js-menu-screen").on("click touchstart",function(e){$("#js-menu, #js-menu-screen").toggleClass("is-visible"),$("#js-menu-trigger").toggleClass("slide close"),e.preventDefault()})}),$(document).ready(function(){$("#main").fitVids()}),$("#markdown-toc").prepend("<li><h6>Overview</h6></li>");
+(function($) {
+	/* ======= Clear Default ====== */
+	$.fn.clearDefault = function() {
+		"use strict";
+		return this.each(function() {
+			var default_value = $(this).val();
+		});
+	};
+
+	/* ======= Height Fix ====== */
+	function vertCenter(item) {
+		"use strict";
+		item.css({
+			'margin-top' : '-' + parseInt((item.height() / 2), 0) + 'px'
+		}).fadeIn();
+	}
+
+	jQuery(window).load(function(){
+		vertCenter($('.itemwrap > li > div'));
+		vertCenter($('#thumbs'));
+		vertCenter($('#clock'));
+		vertCenter($('#demo_thumbs'));
+	});
+
+
+	jQuery(document).ready(function($) {
+		"use strict";
+
+		$('.show_toggle a').click(function(){
+			$('body').toggleClass('menu_show');
+
+			return false;
+		});
+
+		$('.form_submit').click(function(){
+			var form = $(this).parents('form');
+			form.find('.form_item').removeClass('error');
+			form.find('.error_block').remove();
+			var post_data;
+			var errors = formValidation(form),
+				output;
+			if( Object.keys(errors).length > 0 ) {
+				showErrors(form, errors);
+			} else {
+				if(form.attr('id') == 'contacts_form') {
+ 					post_data = {
+            		    'name'     : $('input[name=name]').val(),
+            		    'email'    : $('input[name=email]').val(),
+            		    'message'  : $('input[name=message]').val()
+            		};
+
+            		//Ajax post data to server
+            		jQuery.post('contacts.php', post_data, function(response){	
+            		    if(response.type == 'error'){ //load json data from server and output message    
+            		        output = '<div class="error_block">'+response.text+'</div>';
+            		    } else{
+            		        output = '<div class="success">'+response.text+'</div>';
+            		        //reset values in all input fields
+            		        $("#contacts_form .form_item").val('');
+            		    }
+            		    form.find('.form_row').slideUp();
+            		    form.find("#contact_results").hide().html(output).slideDown();
+            		}, 'json');
+        		} else {
+        			post_data = {
+            		    'subscribe_email': $('input[name=subscribe_email]').val(),
+            		};
+	
+            		jQuery.post('subscribe.php', post_data, function(response){	
+	        	    	   
+        		        output = '<div class="success">'+response.text+'</div>';
+        		        //reset values in all input fields
+        		        $("#contacts_form .form_item").val('');
+        		        form.find('.form_inner').slideUp();
+            		    form.find("#form_results").hide().html(output).slideDown();
+            		}, 'json');
+        		}
+
+		}
+		return false;
+	});
+
+		$('.side-page').click(function() {
+			var curPage = $(this).attr('id');
+			$('.main-menu li').removeClass('active');
+			$('.main-menu li a[data-page="' + curPage + '"]').parent().addClass('active');
+			$('.side-page').removeClass('active').removeClass('went-left').removeClass('went-right');
+			$(this).prev().addClass('side-left').addClass('went-left');
+			$(this).next().addClass('side-right').addClass('went-right');
+			$(this).addClass('active');
+		});
+
+		$('.main-menu a:not(.home-link)').click(function() {
+			$('#clock').removeClass('active');
+			$('.mainarea-content').addClass('active');
+			$('.close').addClass('active');
+			var curPage = $(this).attr('data-page');
+			$('.main-menu li').removeClass('active');
+			$(this).parent().addClass('active');
+			$('.mainarea-content > div').removeClass('active').removeClass('went-left').removeClass('went-right');
+			$('#' + curPage + '').prev().addClass('side-left').addClass('went-left');
+			$('#' + curPage + '').next().addClass('side-right').addClass('went-right');
+			$('#' + curPage + '').addClass('active');
+		});
+
+		$('.close').click(function(e) {
+			e.preventDefault();
+			$('#clock').addClass('active');
+			$('.main-menu li').removeClass('active');
+			$('.close').removeClass('active');
+			$('.mainarea-content').removeClass('active');
+		});
+
+		$('.home-link').click(function(e) {
+			e.preventDefault();
+			$('#clock').addClass('active');
+			$('.main-menu li').removeClass('active');
+			$('.close').removeClass('active');
+			$('.mainarea-content').removeClass('active');
+			$('.home-link').parent().addClass('active');
+		});
+
+		$('input[type="text"]').clearDefault();
+
+
+		$('#tweet_list').cycle({
+			fx : 'custom',
+			cssBefore : {
+				top : 50,
+				height : 100,
+				opacity : 0,
+				display : 'block'
+			},
+			animIn : {
+				top : 0,
+				opacity : 1
+			},
+			animOut : {
+				opacity : 0,
+				top : -50
+			},
+			cssAfter : {
+				zIndex : 0,
+				display : 'none'
+			},
+			speed : 1750,
+			sync : false,
+			easeIn : 'easeOutBack',
+			easeOut : 'easeInBack'
+		});
+	});
+	
+	function resizeStuff() {
+		vertCenter($('.itemwrap > li > div'));
+		vertCenter($('#thumbs'));
+		vertCenter($('#clock'));
+	}
+	var onSmartResize = false;
+
+	$(window).resize(function(){
+		if(onSmartResize !== false)
+			clearTimeout(onSmartResize);
+		onSmartResize = setTimeout( function(){resizeStuff() }, 200); //200 is time in miliseconds
+	});
+
+})(jQuery);
+
+
+
+/* Forms Validation */
+function formValidation(form) {
+
+	var error = {};
+
+	if(form) {
+		form.find('.form_item').each(function(){
+			var $th = $(this);
+
+			if( $th.val() != '' ) {
+				if( $th.attr('type') == 'email' ) {
+					var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+					if( !emailReg.test( jQuery.trim($th.val()) ) ) {
+						error[$th.attr('id')] = 'not_email';
+					}
+				}
+			} else {				
+				error[$th.attr('id')] = 'empty';
+			}
+
+		});
+	}
+	return error;
+}
+
+/* Validation Errors */
+function showErrors(form, errors) {
+	var error_message = ''
+	for(var i in errors) {
+		var form_item = form.find($('#'+i)),
+			form_item_name = form_item.attr('placeholder');
+
+		form_item.addClass('error');
+		if( errors[i] == 'empty' )
+			error_message += '<div class="error">Field '+form_item_name+' is required</div>';
+		else
+			error_message += '<div class="error">You entered an invalid email</div>';
+	}
+	if( form.find('.error_block').length > 0) {
+		form.find('.error_block').html(error_message);
+	} else {
+		form.append('<div class="error_block">'+error_message+'</div>');
+	}
+}
